1f1be76b61e7f88c71f942f2582ef2aa70888866
Mark June <marcusjune95@gmail.com>
2021-04-30 01:27:27 -0400

    updated codes

--- script.js
+++ script.js
@@ -1,33 +1,81 @@
 //current day
-var currentDayEl = $('#currentDay')
-var currentDay = moment().format('dddd, MMMM Do');
-currentDayEl.text(currentDay);
-// console.log(currentDay)
+function setCurrenttDay(){
+    var currentDayEl = $('#currentDay')
+    var currentDay = moment().format('dddd, MMMM Do');
+    currentDayEl.text(currentDay);
 
-//create time block 9am to 5pm 
+}
+
+//html container to append time blocks
 var timeBlockEl = $('.container')
-//create row-div class="row"
-var row = $('<div>')
-row.attr('class', 'row')
 
-//create time-div class="col-1 hour p-0"
-var time = $('<div>')
-time.attr('class', 'col-1 hour p-0')
+//varialbes to create html element
+var row;
+var time;
+var textAreaDiv;
+var submitDiv
 
-//create textArea-div class="col-10"
-var textAreaDiv = $('<div>')
-textAreaDiv.attr('class', 'col-10')
-var textArea = $('<textarea>')
-textArea.attr('class', 'col-11 description')
+//function that creates html element
+function createTimeBlock(){
+    //create row
+    row = $('<div>')
+    row.attr('class', 'row')
 
-//create saveBtn-div
-var submitDiv = $('<div>')
-submitDiv.attr('class', 'col-1 saveBtn')
-var submitBtn = $('<button>')
-submitBtn.attr('class', 'btn')
+    //create time-div class="col-1 hour p-0"
+    time = $('<div>')
+    time.attr('class', 'col-1 hour p-0')
+    
+    //create textArea-div class="col-10"
+    textAreaDiv = $('<div>')
+    textAreaDiv.attr('class', 'col-10')
+    var textArea = $('<textarea>')
+    textArea.attr('class', 'col-11 description')
+    textAreaDiv.append(textArea)
 
-var currentTime = moment().format('h A')
-console.log(currentTime)
+    //create saveBtn-div
+    submitDiv = $('<div>')
+    submitDiv.attr('class', 'col-1 saveBtn')
+    var submitBtn = $('<button>')
+    submitBtn.attr('class', 'btn')
+    var saveSymbol = $('<i>')
+    saveSymbol.attr('class', 'fas i')
+    saveSymbol.html('&#xf0c7;')
+
+    submitBtn.append(saveSymbol)
+    submitDiv.append(submitBtn)
+
+}
+
+//set start/current/end time-block
+var timeBlock;
+var startTimeBlock = parseInt(moment('0', 'h').format('H'))
+var endTimeBlock = parseInt(moment('23', 'h').format('H'))
+var currentTime = parseInt(moment().format('H'))
+
+//function that appends time-blocks to html
+function appendTimeBlock(){
+    for(var i = startTimeBlock; i < endTimeBlock + 1; i++){
+        createTimeBlock()
+        time.text(`${timeBlock = moment(i, 'h').format('h A')}`)
+
+        if(currentTime > i){
+            textAreaDiv.addClass('past')
+        }
+        else if(currentTime < i){
+            textAreaDiv.addClass('future')
+        }
+        else{
+            textAreaDiv.addClass('present')
+        }
+        row.append(time, textAreaDiv, submitDiv)
+        timeBlockEl.append(row)    
+    }
+    
+}
+
+//revoke functions
+setCurrenttDay()
+appendTimeBlock()
 
 
 
