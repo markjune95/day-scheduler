c9cbc90de63127f6074e54bb4015a68b040474fe
Mark June <marcusjune95@gmail.com>
2021-05-05 23:56:46 -0400

    updated script.js

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ var timeBlockEl = $('.container')
 var row;
 var time;
 var textAreaDiv;
-var submitDiv
+var submitDiv;
+var saveSymbol;
+var textArea
 
 //function that creates html element
 function createTimeBlock(){
@@ -28,8 +30,8 @@ function createTimeBlock(){
     //create textArea-div class="col-10"
     textAreaDiv = $('<div>')
     textAreaDiv.attr('class', 'col-10')
-    var textArea = $('<textarea>')
-    textArea.attr('class', 'col-12 description')
+    textArea = $('<textarea>')
+    textArea.addClass('col-12 description')
     textAreaDiv.append(textArea)
 
     //create saveBtn-div
@@ -37,16 +39,19 @@ function createTimeBlock(){
     submitDiv.attr('class', 'col-1 saveBtn')
     var submitBtn = $('<button>')
     submitBtn.attr('class', 'btn')
-    var saveSymbol = $('<i>')
+    saveSymbol = $('<i>')
     saveSymbol.attr('class', 'fas fa-save')
+    
     // saveSymbol.html('&#xf0c7;')
 
     submitBtn.append(saveSymbol)
     submitDiv.append(submitBtn)
+    
 
 }
 
 //set start/current/end time-block
+var timeSlot = []
 var timeBlock;
 var startTimeBlock = parseInt(moment('0', 'h').format('H'))
 var endTimeBlock = parseInt(moment('23', 'h').format('H'))
@@ -54,8 +59,13 @@ var currentTime = parseInt(moment().format('H'))
 
 //function that appends time-blocks to html
 function appendTimeBlock(){
+    
     for(var i = startTimeBlock; i < endTimeBlock + 1; i++){
+        timeSlot.push('')
         createTimeBlock()
+        saveSymbol.attr('save',i)
+        textArea.addClass(`block-${i}`)
+        
         time.text(`${timeBlock = moment(i, 'h').format('h A')}`)
 
         if(currentTime > i){
@@ -68,14 +78,54 @@ function appendTimeBlock(){
             textAreaDiv.addClass('present')
         }
         row.append(time, textAreaDiv, submitDiv)
-        timeBlockEl.append(row)    
+        timeBlockEl.append(row)
     }
+  
+}
+
+function setItem(){
+    localStorage.setItem('timeblock', JSON.stringify(timeSlot))
     
 }
+
+function getItem(){
+    var stored = JSON.parse(localStorage.getItem('timeblock'))
+    timeSlot = stored
+    for(var i = 0; i < endTimeBlock; i++){
+        textAreaEl = $(`.block-${i}`).val(timeSlot[i])
+        
+    }
+    setItem()
+    
+    
+}
+
+var saveSlot;
+var textAreaEl;//html element generated in javascript
+
+timeBlockEl.on('click','.btn',(function(event){
+    event.preventDefault()
+    saveSlot = $(event.target).attr('save')
+    textAreaEl =  $(`.block-${saveSlot}`).val()
+    timeSlot.splice(saveSlot, 1, textAreaEl)//save the value in exact index you want
+    setItem()
+    
+    
+})
+)
+
+
 
 //revoke functions
 setCurrenttDay()
 appendTimeBlock()
+
+
+
+
+
+
+
 
 
 
